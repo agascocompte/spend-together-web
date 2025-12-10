ac93b522a3da3c1b71c949d48bc461cda136de9f
Arturo <a.gascocompte@gmail.com>
2025-12-10 20:08:20 +0100

    added redirect handler

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import RedirectHandler from "./components/RedirectHandler.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename="/spend-together-web">
+      <RedirectHandler />
       <App />
     </BrowserRouter>
   </StrictMode>
